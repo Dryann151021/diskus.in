d6afd403bea15c6595f1dc757e3675aabae73fd8
adriyan <adrianadrian.aa613@gmail.com>
2026-03-16 13:03:34 +0700

    t

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -3,6 +3,10 @@ import { useState } from 'react';
 function useInput(defaultValue = '') {
   const [value, setValue] = useState(defaultValue);
 
+  const handleValueChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return [value, handleValueChange, setValue];
 }
 
